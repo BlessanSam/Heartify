b76e470d9637329a90b8e7ad5022e4e59faf1468
Cod3ist <ZN103@live.mdx.ac.uk>
2023-03-14 22:22:24 +0400

    heartrate page error fixed, UI changes made

--- lib/heartBeat.dart
+++ lib/heartBeat.dart
@@ -18,34 +18,38 @@ class _ViewHeartBeatState extends State<ViewHeartBeat> {
         title: Text('HEARTBEAT'),
         backgroundColor: Colors.teal,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: NetworkImage('https://mir-s3-cdn-cf.behance.net/project_modules/disp/89ba7387618661.5dbdbcfade1a8.gif'),opacity: 150),
-        ),
-         child: Center(
-           child:Column(
-             mainAxisAlignment: MainAxisAlignment.center,
-             crossAxisAlignment: CrossAxisAlignment.center,
-             children: [
-               Text('Your heart rate',style: TextStyle(fontSize: 30,color: Colors.black45),),
-               SizedBox(height: 10,),
-               StreamBuilder(
-                 stream: database.child(widget.uid).child('HeartRate').onValue,
-                 builder: (context,AsyncSnapshot<DatabaseEvent> snapshot){
-                   print(snapshot.data?.snapshot.key.toString());
-                   Map<dynamic, dynamic> map = snapshot.data?.snapshot.value as dynamic;
-                   if(!snapshot.hasData){
-                     return const CircularProgressIndicator();
-                   } else {
-                     return ListTile(
-                       title: Center(child: Text(map['value'].toString(), style: TextStyle(fontSize: 50.0, color: Colors.black45),)),
-                     );
-                   }
-                 },
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: NetworkImage('https://mir-s3-cdn-cf.behance.net/project_modules/disp/89ba7387618661.5dbdbcfade1a8.gif'),opacity: 150),
+            ),
+             child: Center(
+               child:Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 crossAxisAlignment: CrossAxisAlignment.center,
+                 children: [
+                   Text('Your heart rate',style: TextStyle(fontSize: 30,color: Colors.black45),),
+                   SizedBox(height: 10,),
+                   StreamBuilder(
+                     stream: database.child(widget.uid).child('HeartRate').onValue,
+                     builder: (context,AsyncSnapshot<DatabaseEvent> snapshot){
+                       if(snapshot.hasData){
+                         print(snapshot.data?.snapshot.value.toString().runtimeType);
+                         Map<dynamic, dynamic> map = snapshot.data?.snapshot.value as dynamic;
+                         return ListTile(
+                           title: Center(child: Text(map['value'].toString(), style: TextStyle(fontSize: 50.0, color: Colors.black45),)),
+                         );
+                       } else {
+                         return CircularProgressIndicator();
+                       }
+                     },
+                   ),
+                 ],
                ),
-             ],
-           ),
-         ),
+             ),
+          ),
+        ),
       ),
     );
   }
